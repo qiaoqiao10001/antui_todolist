dde5dd86958db6db132ac92b2038c3097b526c1e
qiaoqiao10001 <qiaoqiao10001@gmail.com>
2018-11-09 23:19:22 +0800

    使用redux完成增加list的功能

---
+++ src/store/reducer.js
@@ -0,0 +1,21 @@
+const defaultState = {
+    inputValue:'123',
+    list:[1,2]
+}
+
+//reducer可以接受state，但是不能修改state  所以需要拷贝一份state
+export default (state = defaultState,action) => {
+    console.log(state,action);  //拿到当前要做的事情action内容
+    if(action.type === 'change_input_value'){
+        const newState = JSON.parse(JSON.stringify(state))  //深拷贝state
+        newState.inputValue = action.value;
+        return newState; //把数据返回给store了  store会自动接受这个state  组件中就能使用这个新数据
+    }
+    if(action.type === 'add_todo_item'){
+        const newState = JSON.parse(JSON.stringify(state))
+        newState.list.push(newState.inputValue);
+        newState.inputValue = ''
+        return newState;
+    }
+    return state;
+}
--- src/todolist.js
+++ src/todolist.js
@@ -1,10 +1,52 @@
 import React,{Component} from 'react'
+import 'antd/dist/antd.css'
+import {Input,Button,List} from 'antd'
+import store from './store'
 
 export default class TodoList extends Component{
+    constructor(props){
+        super(props);
+        this.state = store.getState()
+        console.log(store.getState())
+
+        store.subscribe(this.handleStoreChange) //订阅store里面的内容
+    }
+
+    handleInputChange = (e) => {
+        const action = {
+            type:'change_input_value',
+            value:e.target.value
+        }
+        store.dispatch(action);
+    }
+
+    handleStoreChange = () => {
+        console.log('store change')
+        this.setState(store.getState())
+    }
+
+    handleAddList = () => {
+        const action = {
+            type: 'add_todo_item'
+        }
+        store.dispatch(action)
+    }
+
     render(){
         return(
-            <div>
-                <input type="text"/>
+            <div style={{margin:'10px 20px'}}>
+                <Input type="text"
+                       value={this.state.inputValue}
+                       style={{width:'400px',marginRight:'10px'}}
+                       onChange = {this.handleInputChange}
+                />
+                <Button type="primary" onClick={this.handleAddList}>提交</Button>
+                <List
+                    style={{width:'400px'}}
+                    bordered
+                    dataSource={this.state.list}
+                    renderItem={item => (<List.Item>{item}</List.Item>)}
+                />
             </div>
         )
     }
