8744ce2a4b9e999205e48d9c5724596874149ea0
qiaoqiao10001 <qiaoqiao10001@gmail.com>
2018-11-10 09:54:28 +0800

    单独拆分actionType到一个文件

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,3 +1,5 @@
+import {CHANGE_INPUT_VALUE,ADD_TODO_ITEM,DELETE_ITEM} from './actionTypes.js'
+
 const defaultState = {
     inputValue:'',
     list:[]
@@ -6,18 +8,18 @@ const defaultState = {
 //reducer可以接受state，但是不能修改state  所以需要拷贝一份state
 export default (state = defaultState,action) => {
     console.log(state,action);  //拿到当前要做的事情action内容
-    if(action.type === 'change_input_value'){
+    if(action.type === CHANGE_INPUT_VALUE){
         const newState = JSON.parse(JSON.stringify(state))  //深拷贝state
         newState.inputValue = action.value;
         return newState; //把数据返回给store了  store会自动接受这个state  组件中就能使用这个新数据
     }
-    if(action.type === 'add_todo_item'){
+    if(action.type === ADD_TODO_ITEM){
         const newState = JSON.parse(JSON.stringify(state))
         newState.list.push(newState.inputValue);
         newState.inputValue = ''
         return newState;
     }
-    if(action.type === 'delete_item'){
+    if(action.type === DELETE_ITEM){
         const newState = JSON.parse(JSON.stringify(state))
         newState.list.splice(action.index,1)
         return newState;
--- src/todolist.js
+++ src/todolist.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react'
 import 'antd/dist/antd.css'
 import {Input,Button,List} from 'antd'
 import store from './store'
+import {CHANGE_INPUT_VALUE,ADD_TODO_ITEM,DELETE_ITEM} from './store/actionTypes.js'
 
 export default class TodoList extends Component{
     constructor(props){
@@ -14,7 +15,7 @@ export default class TodoList extends Component{
 
     handleInputChange = (e) => {
         const action = {
-            type:'change_input_value',
+            type:CHANGE_INPUT_VALUE,
             value:e.target.value
         }
         store.dispatch(action);
@@ -27,14 +28,14 @@ export default class TodoList extends Component{
 
     handleAddList = () => {
         const action = {
-            type: 'add_todo_item'
+            type: ADD_TODO_ITEM
         }
         store.dispatch(action)
     }
 
     handleItemDelete = (index) => {
         const action = {
-            type:'delete_item',
+            type:DELETE_ITEM,
             index
         }
         store.dispatch(action)
